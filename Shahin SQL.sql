93de9718a69520165561d2b322e0ddb615d28f77
Garibzade <161576671+Garibzade@users.noreply.github.com>
2024-04-30 02:37:51 +0400

    Added First

---
+++ Shahin SQL.sql
@@ -0,0 +1,29 @@
+﻿--CREATE DATABASE AB106
+--USE AB106
+
+--CREATE TABLE Students(
+--[NAME] NVARCHAR(20) NOT NULL,
+--SURNAME NVARCHAR (25) NULL DEFAULT 'XXX',
+--AGE INT CHECK(AGE>=18),
+--AVGPOINT FLOAT CHECK(AVGPOINT BETWEEN 0 AND 100)
+--)
+
+--INSERT INTO Students VALUES
+--(N'Şahin',N'Əlizadə',20,99),
+--(N'Qərib',N'Qəribzadə',21,100),
+--('Emil',N'Paşayev',20,98),
+--('Ali',N'Sükürov',25,50),
+--(N'Rauf',N'Dosduyev',19,85),
+--(N'Samir',N'Kərimov',19,99)
+
+--SELECT * FROM Students
+
+
+--SELECT * FROM Students WHERE AVGPOINT > 51
+
+
+--SELECT * FROM Students Where AVGPOINT>51 and AVGPOINT<90
+
+--SELECT * FROM Students WHERE Name LIKE 'A%' AND Name LIKE '%i'
+
+ 
